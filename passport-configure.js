6c11b6e9c62955534def856aeca82b1fa74077f7
NTrope <ju5t1n5t0n3@gmail.com>
2015-08-11 21:25:29 -0500

    passport configuration file was enclosed in a function and made module.exports for better usage in app.js

--- passport-configure.js
+++ passport-configure.js
@@ -1,4 +1,4 @@
-var passport = require('passport');
+/* globals module: false, require: false, console: false */
 var LocalStrategy = require('passport-local');
 var async = require('async');
 var mongoose = require('mongoose');
@@ -7,46 +7,43 @@ var User = mongoose.model('User');
  auth.js is used for passport configuration
  =======================================================================*/
 
-passport.use(new LocalStrategy(function (username, password, done) {
+module.exports = function (passport) {
     'use strict';
+    passport.use(new LocalStrategy(function (username, password, done) {
+        var findUser = User.findOne({username: username}).exec();
 
-    var findUser = User.findOne({username: username}).exec();
-
-    var verifyUserPassword = findUser.then(function(user){
-        return new Promise(function(resolve, reject) {
-            if (!user) {
-                // Password can't match against a nonexistent user's password
-                return resolve({isMatch:false, user:user});
-            }
-            user.verifyPassword(password, function (err, isMatch) {
-                if (err) {
-                    return reject(err);
+        var verifyUserPassword = findUser.then(function (user) {
+            return new Promise(function (resolve, reject) {
+                if (!user) {
+                    // Password can't match against a nonexistent user's password
+                    return resolve({isMatch: false, user: user});
                 }
-                resolve({isMatch:isMatch, user:user});
+                user.verifyPassword(password, function (err, isMatch) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve({isMatch: isMatch, user: user});
+                });
             });
+        }, function (err) {
+            return done(err);
         });
-    }, function(err) {
-        return done(err);
+
+        verifyUserPassword.then(function (results) {
+            return done(null, results.isMatch ? results.user : false);
+        }, function (err) {
+            return done(err);
+        });
+
+    }));
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.username);
     });
 
-    verifyUserPassword.then(function(results){
-        return done(null, results.isMatch ? results.user : false)
-    }, function(err){
-        return done(err);
+    passport.deserializeUser(function (username, done) {
+        User.findOne({username: username}, function (err, user) {
+            done(err, user);
+        });
     });
-
-}));
-
-passport.serializeUser(function (user, done) {
-    'use strict';
-    done(null, user.username);
-});
-
-passport.deserializeUser(function (username, done) {
-    'use strict';
-    User.findOne({username: username}, function (err, user) {
-        done(err, user);
-    });
-});
-
-module.exports = passport;
+};
