036f70cd0ab04fbc6f0614774716fa0e7bcfffdd
NTrope <ju5t1n5t0n3@gmail.com>
2015-08-11 21:17:45 -0500

    Added session namespace configuration to socket.io

---
+++ session-io.js
@@ -0,0 +1,13 @@
+/* globals console: false, module: false, require: false */
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+module.exports = function(io){
+    'use strict';
+
+    var sessionNSP = io.of('session');
+
+    sessionNSP.on('connection', function(socket) {
+        console.log(socket.request.user);
+    });
+};
